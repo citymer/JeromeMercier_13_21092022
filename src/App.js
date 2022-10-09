ad938ac4e9ced2b1772a991ed36c47a2d6e04891
citymer <cityhunter8484@gmail.com>
2022-10-09 09:59:41 +0200

    securité login rectifié

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react'
-//import logo from './logo.svg'
-//import { Counter } from './features/counter/Counter'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import '../src/styles/index.scss'
+import { useDispatch, useStore } from 'react-redux'
+import { authFetching } from './features/connextionSlice'
+import { getUserProfil } from './services/API'
 
 function App() {
+  const dispatch = useDispatch()
+  const store = useStore()
+
+  useEffect(() => {
+    const checkAutoLogin = (store, dispatch) => {
+      const tokenString = localStorage.getItem('token')
+      if (!tokenString) {
+        return
+      }
+      const token = JSON.parse(tokenString)
+      dispatch(authFetching())
+      getUserProfil(store, token)
+    }
+
+    checkAutoLogin(store, dispatch)
+  })
   return (
     <BrowserRouter>
       <Routes>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 import Welcome from '../components/Welcome'
 import Footer from '../components/Footer'
 import HeaderProfile from '../components/HeaderProfile'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
 
 const Profile = () => {
-  return (
-    <div>
-      <HeaderProfile />
-      <Welcome />
-      <Footer />
-    </div>
-  )
+  const logged = useSelector((state) => state.isAuthenticate)
+  const token = JSON.parse(localStorage.getItem('token'))
+
+  if (!logged && !token) {
+    return <Navigate to="/" />
+  } else {
+    return (
+      <div>
+        <HeaderProfile />
+        <Welcome />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default Profile
